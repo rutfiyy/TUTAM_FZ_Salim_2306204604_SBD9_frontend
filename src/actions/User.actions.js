218c9490596cef833742b4fe8bffbff58d169f59
Salim31 <salimalhaddad1211@gmail.com>
2025-05-04 19:23:40 +0700

    TUTAM9 - Perbaikan URL

--- src/actions/User.actions.js
+++ src/actions/User.actions.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'tutam-fz-salim-2306204604-sbd-9-backend-jbmoj44tx.vercel.app';
+const BASE_URL = 'tutam-fz-salim-2306204604-sbd-9-backend.vercel.app';
 
 // Function to fetch all cards
 export const fetchCards = async () => {
